095917ae5b4326505b7a957e0979993056f20b75
Nika Kopadze <nikolozkopadze@gmail.com>
2023-08-06 09:39:48 +0400

    clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,5 @@
-'use client';
-import { authOptions } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
-import { signOut, useSession } from 'next-auth/react';
-
 export default function Home() {
-  // const session = await getServerSession(authOptions);
-  const { data: session } = useSession();
-
-  // if (!session) {
-  //   redirect('/login');
-  // }
-
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-4xl font-bold text-center'>Welcome to NextAuth.js</h1>
-      <p className='text-center'>
-        <button
-          className='text-blue-600 hover:text-blue-700'
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
-      </p>
-    </main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
   );
 }
